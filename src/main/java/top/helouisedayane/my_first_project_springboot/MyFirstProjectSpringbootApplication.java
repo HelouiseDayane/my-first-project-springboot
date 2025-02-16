018b79f40e1e535412debba18084d3b62fb54910
helouise <helouisedayane@gmail.com>
2025-02-16 12:13:27 -0300

    escolpo

---
+++ src/main/java/top/helouisedayane/my_first_project_springboot/MyFirstProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package top.helouisedayane.my_first_project_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/helouisedayane/my_first_project_springboot/MyFirstProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package top.helouisedayane.my_first_project_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
